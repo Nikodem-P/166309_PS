1b5255c9913cbb2fb533f2478efadeb9ab3f0f10
Nikodem <97964965+Nikodem-P@users.noreply.github.com>
2022-03-03 15:50:54 +0100

    Create 1_2_10.c

---
+++ lab2/1_2_10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float liczba;
+    printf("Podaj liczbe wymierna: ");
+    scanf("%f", &liczba);
+    if (liczba < 0)
+        liczba *= -1;
+    printf("Wartosc bezwzgledna liczby: %f", liczba);
+}
