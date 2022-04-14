f3368359503a739404eba679e1a278d69639d3e8
Nikodem <97964965+Nikodem-P@users.noreply.github.com>
2022-04-14 15:46:34 +0200

    Create 5_2_5.c

---
+++ lab8/5_2_5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void przepisz(char*, char*);
+void przepisz(wchar_t*, wchar_t*);
+
+
+int main()
+{
+    char str1[] = "agata";
+    char str2[10];
+
+    przepisz(str1, str2);
+    printf("%s", str2);
+
+    return 0;
+}
+
+void przepisz(char* napis1, char* napis2)
+{
+    while (*napis1 != '\0')
+    {
+        *napis2 = *napis1;
+        napis1++;
+        napis2++;
+    }
+}
+
+void przepisz(wchar_t* napis1, wchar_t* napis2)
+{
+    while (*napis1 != '\0')
+    {
+        *napis2 = *napis1;
+        napis1++;
+        napis2++;
+    }
+}
+
