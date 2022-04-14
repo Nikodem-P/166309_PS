2f9a41fca377912ff678ecf3d15a6fc77dbdf494
Nikodem <97964965+Nikodem-P@users.noreply.github.com>
2022-04-14 15:32:03 +0200

    Update 5_2_3.c
    wchar_t - nie wiem czym to sie rozni od zwyklego char'a

--- lab8/5_2_3.c
+++ lab8/5_2_3.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 int porownaj(char*, char*);
+int porownaj(wchar_t*, wchar_t*);
 
 int main()
 {
@@ -30,3 +31,23 @@ int porownaj(char* napis1, char* napis2)
     }
     return 1;
 }
+
+int porownaj(wchar_t* napis1, wchar_t* napis2)
+{
+    wchar_t* n1 = napis1;
+    wchar_t* n2 = napis2;
+
+    while ((*n1 != '\0') || (*n2 != '\0'))
+    {
+        if (*n1 = *n2)
+        {
+            n1++;
+            n2++;
+            continue;
+        }
+        else
+            return 0;
+    }
+    return 1;
+}
+
